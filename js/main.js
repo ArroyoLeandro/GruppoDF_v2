9fcf56c457ee8bcbf15bab41fb9af55846787ba6
ArroyoLeandro <martin_arroyo@hotmail.es>
2020-08-11 21:25:02 -0300

    servicios circulares

--- js/main.js
+++ js/main.js
@@ -66,6 +66,51 @@ window.onscroll=function(){
         }
     });
 
+
+
+
+  /*====================================
+			Circular Services
+======================================*/
+
+
+var angleStart = -360;
+        
+// jquery rotate animation
+function rotate(li,d) {
+    $({d:angleStart}).animate({d:d}, {
+        step: function(now) {
+            $(li)
+               .css({ transform: 'rotate('+now+'deg)' })
+               .find('label')
+                  .css({ transform: 'rotate('+(-now/2)+'deg)' });
+        }, duration: 0
+    });
+}
+
+// show / hide the options
+function toggleOptions(s) {
+    $(s).toggleClass('open');
+    var li = $(s).find('li');
+    var deg = $(s).hasClass('half') ? 180/(li.length-1) : 360/li.length;
+    for(var i=0; i<li.length; i++) {
+        var d = $(s).hasClass('half') ? (i*deg)-90 : i*deg;
+        $(s).hasClass('open') ? rotate(li[i],d) : rotate(li[i],angleStart);
+    }
+}
+
+//si activo esta funcion el user puede cerrar y abrir los circulos
+$('.selector button').click(function(e) {
+    toggleOptions($(this).parent());
+});
+
+setTimeout(function() { toggleOptions('.selector'); }, 100);
+
+
+
+
+
+
 });
 
 
@@ -189,3 +234,4 @@ update = function () {
     requestAnimationFrame(update);
 };
 requestAnimationFrame(update);
+
